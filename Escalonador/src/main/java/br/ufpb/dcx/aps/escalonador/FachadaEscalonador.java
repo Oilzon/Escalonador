16d7d109a6bf5f267fef17480647931f4eda8192
Francisco Ursulino <francisco.ursulino@dcx.ufpb.br>
2019-12-01 14:46:50 -0300

    T07 pronto

---
+++ Escalonador/src/main/java/br/ufpb/dcx/aps/escalonador/FachadaEscalonador.java
@@ -0,0 +1,43 @@
+package br.ufpb.dcx.aps.escalonador;
+
+public class FachadaEscalonador {
+	
+	private TipoEscalonador tipoEscalonador;
+	private int tick;
+
+	public FachadaEscalonador(TipoEscalonador tipoEscalonador) {
+		this.tipoEscalonador = tipoEscalonador;
+	}
+
+	public FachadaEscalonador(TipoEscalonador roundrobin, int quantum) {
+	}
+
+	public String getStatus() {
+		return "Escalonador RoundRobin;Processos: {};Quantum: 3;Tick: " + tick;
+	}
+
+	public void tick() {
+		tick++;
+	}
+
+	public void adicionarProcesso(String nomeProcesso) {
+		
+	}
+
+	public void adicionarProcesso(String nomeProcesso, int prioridade) {
+	}
+
+	public void finalizarProcesso(String nomeProcesso) {
+	}
+
+	public void bloquearProcesso(String nomeProcesso) {
+	}
+
+	public void retomarProcesso(String nomeProcesso) {
+		
+	}
+
+	public void adicionarProcessoTempoFixo(String string, int duracao) {
+		
+	}
+}
